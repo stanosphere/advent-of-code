80f8dddfe2f4b7fb7529e2e037087e6bcfb69795
paul <pmstanford95@gmail.com>
2023-11-19 20:55:15 +0000

    add ittereatetofiedPoint function

--- src/2018/Day5.hs
+++ src/2018/Day5.hs
@@ -1,22 +1,27 @@
 module Day5 where
 
 import Data.Char (toUpper)
-import Data.Foldable (traverse_)
 
 part1 :: IO ()
 part1 = do
   inp <- readFile "./fixtures/input5.txt"
-  let res = iterate annihilate inp
-  traverse_ (print . length) . take 500 $ res
+  let res = iteratetoFixedPoint annihilate inp
+  print . length $ res
 
 -- a bit brute forycy but does the trick; 11668 is the answer
 annihilate :: String -> String
 annihilate [] = []
-annihilate [x] = [x]
-annihilate (x : y : t) =
-  if shouldAnnihilate x y
-    then annihilate t
-    else x : annihilate (y : t)
+annihilate [x0] = [x0]
+annihilate (x0 : x1 : xs)
+  | shouldAnnihilate x0 x1 = annihilate xs
+  | otherwise = x0 : annihilate (x1 : xs)
+
+iteratetoFixedPoint :: Eq a => (a -> a) -> a -> a
+iteratetoFixedPoint f x
+  | x' == x = x'
+  | otherwise = iteratetoFixedPoint f x'
+  where
+    x' = f x
 
 shouldAnnihilate :: Char -> Char -> Bool
 shouldAnnihilate c1 c2 = c1 /= c2 && toUpper c1 == toUpper c2
