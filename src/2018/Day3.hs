3a9ca07316ef300d28a12935f20d08f024d2399e
paul <pmstanford95@gmail.com>
2023-12-02 17:47:50 +0000

    practice my parseccing a bit more!

--- src/2018/Day3.hs
+++ src/2018/Day3.hs
@@ -1,9 +1,8 @@
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
 
 module Day3 where
 
 import Data.Foldable (find)
-import Data.List.Split (splitOn)
 import Data.Map qualified as M
   ( Map,
     elems,
@@ -19,12 +18,14 @@ import Data.Set qualified as S
     size,
     union,
   )
+import Text.Parsec qualified as P
+import Text.ParserCombinators.Parsec (Parser, parse)
 import Prelude hiding (id)
 
 data Coords = Coords {x :: Int, y :: Int} deriving (Eq, Ord, Show)
 
 data Rect = Rect
-  { id :: String,
+  { id :: Int,
     x0 :: Int,
     y0 :: Int,
     width :: Int,
@@ -33,7 +34,7 @@ data Rect = Rect
   deriving (Show)
 
 -- for each sqaure who has claimed it?
-type Squares = M.Map Coords (S.Set String)
+type Squares = M.Map Coords (S.Set Int)
 
 part1 :: IO ()
 part1 = do
@@ -50,7 +51,7 @@ getLines filePath = fmap lines (readFile filePath)
 
 -- so what I have here is sets that tell me about which claims overlap
 -- so if i deduplicate it and then make a list of stuff that never appears in sets larger than one
-findClaimsWithNoOveralps :: Squares -> Maybe String
+findClaimsWithNoOveralps :: Squares -> Maybe Int
 findClaimsWithNoOveralps =
   fmap fst
     . find (not . snd)
@@ -59,7 +60,7 @@ findClaimsWithNoOveralps =
     . map setToMap
     . M.elems
 
-setToMap :: S.Set String -> M.Map String Bool
+setToMap :: S.Set k -> M.Map k Bool
 setToMap set = M.fromSet (const (S.size set > 1)) set
 
 countOverlaps :: Squares -> Int
@@ -69,12 +70,7 @@ aggregateOverlaps :: [Rect] -> Squares
 aggregateOverlaps = foldl (M.unionWith S.union) M.empty . map toSquares
 
 parseRect :: String -> Rect
-parseRect s =
-  let [id, rest] = splitOn " @ " s
-      [xy, wh] = splitOn ": " rest
-      [x0, y0] = splitOn "," xy
-      [w, h] = splitOn "x" wh
-   in Rect id (read x0) (read y0) (read x0 + read w - 1) (read y0 + read h - 1)
+parseRect = unsafeParse rectParser
 
 toSquares :: Rect -> Squares
 toSquares (Rect id x0 y0 x1 y1) =
@@ -83,3 +79,28 @@ toSquares (Rect id x0 y0 x1 y1) =
       | x <- [x0 .. x1],
         y <- [y0 .. y1]
     ]
+
+-- Parsing stuff below here
+unsafeParse :: Parser a -> String -> a
+unsafeParse p s = case parse p "still don't really know wht this arg is for lol" s of
+  Left res -> error . show $ res
+  Right res -> res
+
+rectParser :: Parser Rect
+rectParser = do
+  P.char '#'
+  rectId <- intParser
+  P.string " @ "
+  x0 <- intParser
+  P.char ','
+  y0 <- intParser
+  P.string ": "
+  w <- intParser
+  let x1 = x0 + w - 1
+  P.char 'x'
+  h <- intParser
+  let y1 = y0 + h - 1
+  return (Rect rectId x0 y0 x1 y1)
+
+intParser :: Parser Int
+intParser = read <$> P.many1 P.digit
