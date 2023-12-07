27d99d1b860556588266022f2acebd1139a082e8
paul <pmstanford95@gmail.com>
2023-12-07 01:39:23 +0000

    use quadratic powers

--- src/2023/Day6.hs
+++ src/2023/Day6.hs
@@ -2,6 +2,11 @@ module Day6 where
 
 data Race = Race {time :: Int, distance :: Int}
 
+data Race' = Race' {time' :: Double, distance' :: Double}
+
+coerceRace :: Race -> Race'
+coerceRace (Race t d) = Race' (fromIntegral t) (fromIntegral d)
+
 -- 0.01 secs
 -- 741000
 part1 :: Int
@@ -13,6 +18,14 @@ part1 = product . map getWins $ races
 part2 :: Int
 part2 = getWins oneTrueRace
 
+-- 0.01 secs
+part1' :: Integer
+part1' = product . map (getWins' . coerceRace) $ races
+
+-- 0.00 secs
+part2' :: Integer
+part2' = getWins' . coerceRace $ oneTrueRace
+
 races :: [Race]
 races =
   [ Race 47 207,
@@ -23,6 +36,12 @@ races =
 
 oneTrueRace :: Race
 oneTrueRace = Race 47847467 207139412091014
+
+getWins' :: Integral a => Race' -> a
+getWins' (Race' raceTime dist) = upper - lower
+  where
+    upper = floor ((raceTime + sqrt (raceTime ^ 2 - 4 * dist)) / 2.0)
+    lower = floor ((raceTime - sqrt (raceTime ^ 2 - 4 * dist)) / 2.0)
 
 getWins :: Race -> Int
 getWins (Race raceTime dist) = length . filter (> dist) . map getDistance $ [0 .. raceTime]
