259c5a10f9ec2ebd54d096c00438d756c672868b
paul <pmstanford95@gmail.com>
2023-12-08 22:35:03 +0000

    solve part 2 day 8 but i looked at the subreddit

--- src/2023/Day8.hs
+++ src/2023/Day8.hs
@@ -1,23 +1,46 @@
 module Day8 where
 
-import Data.Foldable
-import Data.Map qualified as M (Map, fromList, (!))
+import Data.Foldable (find)
+import Data.Map qualified as M (Map, fromList, keys, (!))
+import Data.Maybe (mapMaybe)
 import Text.Parsec qualified as P
 import Text.ParserCombinators.Parsec (Parser, parse)
 
 -- 0.04 secs
 -- 18113
-solve :: IO (Maybe (Integer, String))
-solve = do
+solvePart1 :: IO (Maybe Integer)
+solvePart1 = do
   xs <- getLines "./fixtures/input8.txt"
   let infiniteSeq = concat . repeat . head $ xs
   let nodes = M.fromList . map (unsafeParse branchParser) . drop 2 $ xs
-  return . find ((== "ZZZ") . snd) . zip [0 ..] . go nodes $ infiniteSeq
+  return . fmap fst . find ((== "ZZZ") . snd) . zip [0 ..] . go "AAA" nodes $ infiniteSeq
+
+-- hmm sooooo online they're all using LCM
+-- but really in order to use it you should show that the cycles really are synchronised...
+-- anyway I guess I'll just do that
+
+-- 12315788159977
+-- 0.08 secs
+solvePart2 :: IO Integer
+solvePart2 = do
+  xs <- getLines "./fixtures/input8.txt"
+  let infiniteSeq = concat . repeat . head $ xs
+  let nodes = M.fromList . map (unsafeParse branchParser) . drop 2 $ xs
+  return
+    . foldl1 lcm
+    . mapMaybe
+      ( fmap fst
+          . (find ((== 'Z') . last . snd) . zip [0 ..])
+          . (\startingPoint -> go startingPoint nodes infiniteSeq)
+      )
+    . filter ((== 'A') . last)
+    . M.keys
+    $ nodes
 
 data Branch = Branch {left :: String, right :: String} deriving (Show)
 
-go :: M.Map String Branch -> String -> [String]
-go mp = scanl step "AAA"
+go :: String -> M.Map String Branch -> String -> [String]
+go start mp = scanl step start
   where
     step :: String -> Char -> String
     step s 'R' = right (mp M.! s)
