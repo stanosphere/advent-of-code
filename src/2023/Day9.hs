44ea5782c554e98c6ec37947dc627485ef15b629
paul <pmstanford95@gmail.com>
2023-12-09 15:23:23 +0000

    make code tiny bit shorter

--- src/2023/Day9.hs
+++ src/2023/Day9.hs
@@ -4,31 +4,26 @@ import Data.List (tails)
 import Data.List.Split (splitOn)
 
 part1 :: IO Int
-part1 = sum . map (getLastNum . parseLine) <$> inputLines
+part1 = sum . map (getLastNum . parseLine) . lines <$> readFile "./fixtures/input9.txt"
 
 part2 :: IO Int
-part2 = sum . map (getLastNum . reverse . parseLine) <$> inputLines
-
-inputLines :: IO [String]
-inputLines = getLines "./fixtures/input9.txt"
+part2 = sum . map (getLastNum . reverse . parseLine) . lines <$> readFile "./fixtures/input9.txt"
 
 parseLine :: String -> [Int]
 parseLine = map read . splitOn " "
 
-getLines :: FilePath -> IO [String]
-getLines filePath = fmap lines (readFile filePath)
-
 getLastNum :: [Int] -> Int
 getLastNum = sum . map last . allDiffs
+  where
+    allDiffs :: [Int] -> [[Int]]
+    allDiffs = takeWhile (not . all (== 0)) . iterate diffs
 
-allDiffs :: [Int] -> [[Int]]
-allDiffs = takeWhile (not . all (== 0)) . iterate diffs
+    diffs :: [Int] -> [Int]
+    diffs = map (uncurry . flip $ (-)) . pairs
 
-diffs :: [Int] -> [Int]
-diffs = map (uncurry (-)) . pairs
+    pairs :: [a] -> [(a, a)]
+    pairs = map (\[x1, x2] -> (x1, x2)) . windows 2
 
-pairs :: [a] -> [(a, a)]
-pairs = map (\[x1, x2] -> (x2, x1)) . windows 2
-
-windows :: Int -> [a] -> [[a]]
-windows n = takeWhile ((n ==) . length) . map (take n) . tails
+    -- essentially scala's sliding, kinda surprised I couldn't find a library function
+    windows :: Int -> [a] -> [[a]]
+    windows n = takeWhile ((n ==) . length) . map (take n) . tails
