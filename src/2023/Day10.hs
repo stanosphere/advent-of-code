d8e6d3f447af77033369a1b49a52c26b9864ffae
paul <pmstanford95@gmail.com>
2023-12-10 13:59:53 +0000

    pretty print my curve

--- src/2023/Day10.hs
+++ src/2023/Day10.hs
@@ -1,7 +1,7 @@
 module Day10 where
 
 import Data.Foldable
-import Data.Map qualified as M (Map, fromList, toList, (!))
+import Data.Map qualified as M (Map, findWithDefault, fromList, toList, (!))
 
 type SymbolMap = M.Map Coords Char
 
@@ -10,6 +10,9 @@ type Coords = (Int, Int)
 data Direction = N | E | S | W deriving (Show)
 
 data State = State {direction :: Direction, position :: Coords} deriving (Show)
+
+-- so for part 2 I reckon we ca use the Jordan Curve Theorem
+-- https://en.wikipedia.org/wiki/Jordan_curve_theorem
 
 evolveState :: SymbolMap -> State -> State
 evolveState sm (State dir pos) = nextState pos dir (sm M.! pos)
@@ -31,6 +34,15 @@ nextState (x, y) W '─' = State W (x - 1, y)
 nextState (x, y) N '│' = State N (x, y - 1)
 nextState (x, y) S '│' = State S (x, y + 1)
 nextState _ _ _ = undefined
+
+getCurve :: IO SymbolMap
+getCurve = do
+  input <- getLines "./fixtures/input10.txt"
+  let symbols = getSymbolCoords input
+  let startingCoords = (24, 76)
+  let otherPoints = takeWhile ((/= startingCoords) . position) . iterate (evolveState symbols) $ State W (23, 76)
+  let curve = M.fromList (((24, 76), '┐') : map (\(State _ pos) -> (pos, symbols M.! pos)) otherPoints)
+  return curve
 
 -- 6842
 part1 :: IO ()
@@ -62,7 +74,7 @@ getSymbolCoords inp = M.fromList [((x, y), prettifySymbol c) | (y, xs) <- zip [0
     prettifySymbol '-' = '─'
     prettifySymbol '|' = '│'
     prettifySymbol '.' = ' '
-    prettifySymbol 'S' = 'S'
+    prettifySymbol 'S' = '┐'
 
 getLines :: FilePath -> IO [String]
 getLines filePath = fmap lines (readFile filePath)
@@ -71,6 +83,11 @@ prettyPrintSymbolMap :: Int -> SymbolMap -> IO ()
 prettyPrintSymbolMap size mp =
   let counter = [0 .. size]
    in traverse_ putStrLn [[mp M.! (x, y) | x <- counter] | y <- counter]
+
+prettyPrintCurve :: Int -> SymbolMap -> IO ()
+prettyPrintCurve size mp =
+  let counter = [0 .. size]
+   in traverse_ putStrLn [[M.findWithDefault ' ' (x, y) mp | x <- counter] | y <- counter]
 
 get :: Maybe a -> a
 get Nothing = undefined
