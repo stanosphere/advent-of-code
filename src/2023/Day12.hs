3d48bc7fe0fa620b0d9258e87d8c62360ba910e8
paul <pmstanford95@gmail.com>
2023-12-12 22:04:00 +0000

    was indeed doable via brute force

--- src/2023/Day12.hs
+++ src/2023/Day12.hs
@@ -1,24 +1,30 @@
 module Day12 where
 
-import Data.Foldable
+import Data.List (group)
 import Text.Parsec qualified as P
 import Text.ParserCombinators.Parsec (Parser, parse)
 
-part1 = do
-  xs <- getLines "./fixtures/input12.txt"
-  print . sum . map ((2 ^) . length . filter (== '?') . fst . parseLine) $ xs
-
---   traverse_ print ys
 -- so according to my calculations this should be doable vai brute force
 -- would take 7,245,284 iterations for the real input
 -- got this number by doing `print . sum . map ((2 ^) . length . filter (== '?') . fst . parseLine) $ xs`
+-- 7857
+-- (8.67 secs, 15,186,622,288 bytes)
+part1 :: IO Int
+part1 = do
+  xs <- getLines "./fixtures/input12.txt"
+  return . sum . map (uncurry countValid . parseLine) $ xs
 
-go :: String -> [String]
-go [] = [[]]
-go ('?' : t) = concatMap (\x -> ['#' : x, '.' : x]) (go t)
-go (h : t) = map (h :) (go t)
+countValid :: [Int] -> String -> Int
+countValid is = length . filter isValid . go
+  where
+    go :: String -> [String]
+    go [] = [[]]
+    go ('?' : t) = concatMap (\x -> ['#' : x, '.' : x]) (go t)
+    go (h : t) = map (h :) (go t)
+    isValid :: String -> Bool
+    isValid s = is == (map length . filter (\x -> head x == '#') . group $ s)
 
-parseLine :: String -> (String, [Int])
+parseLine :: String -> ([Int], String)
 parseLine = unsafeParse lineParser
 
 unsafeParse :: Parser a -> String -> a
@@ -26,12 +32,12 @@ unsafeParse p s = case parse p "whatever" s of
   Left res -> error . show $ res
   Right res -> res
 
-lineParser :: Parser (String, [Int])
+lineParser :: Parser ([Int], String)
 lineParser = do
   res <- P.many springParser
   _ <- P.space
   numbers <- P.sepBy intParser (P.char ',')
-  return (res, numbers)
+  return (numbers, res)
 
 springParser :: Parser Char
 springParser = P.oneOf "#.?"
