3908f6fc99dfca94cfd0d758d5947cbff6f7b0e6
paul <pmstanford95@gmail.com>
2023-12-13 23:22:20 +0000

    part 2 for day 13

--- src/2023/Day13.hs
+++ src/2023/Day13.hs
@@ -1,68 +1,65 @@
 module Day13 where
 
-import Data.Foldable
+import Data.Foldable (Foldable (toList))
 import Data.List (transpose)
-import Data.List.Split (chunksOf, splitOn)
-import Data.Sequence qualified as S (Seq, adjust', fromList, update)
+import Data.List.Split (splitOn)
+import Data.Sequence qualified as S (Seq, adjust', fromList)
+import Data.Set qualified as ST (Set, difference, fromList, map, union)
 
 type Pattern = [[Char]]
 
-data Reflections = Reflections {vertical :: [Int], horizontal :: [Int]} deriving (Show, Eq)
-
--- print . length . concat . concat $ patterns
--- give me 16,394 so I reckon that's brute forcible
-
-showPattern :: Pattern -> IO ()
-showPattern xs = putStrLn "" *> traverse_ putStrLn xs
-
-part2 = do
-  patterns <- getPatterns "./fixtures/input13Toy.txt"
-  let firstPattern = ["##", ".."]
-  let repacememtPatterns = getAllReplacements firstPattern
-  let res = map findNewRefl patterns
-  traverse_ print res
-
-findNewRefl :: Pattern -> [Reflections]
-findNewRefl p =
-  filter (/= originalRefls)
-    . filter (\(Reflections v h) -> not . null $ (v ++ h))
-    . map getBothReflections
-    . getAllReplacements
-    $ p
-  where
-    originalRefls = getBothReflections p
-
-swapChar :: Char -> Char
-swapChar '#' = '.'
-swapChar '.' = '#'
-swapChar _ = undefined
-
-getAllReplacements :: Pattern -> [Pattern]
-getAllReplacements ps =
-  let asSeq = S.fromList . map S.fromList $ ps
-      updates = [(x, y) | (y, xs) <- zip [0 ..] ps, (x, _) <- zip [0 ..] xs]
-      res = map (`applyUpdate` asSeq) $ updates
-      res' = map (map toList . toList) res
-   in res'
-
-applyUpdate :: (Int, Int) -> S.Seq (S.Seq Char) -> S.Seq (S.Seq Char)
-applyUpdate (x, y) = S.adjust' (S.adjust' swapChar x) y
+data Reflections = Reflections {vertical :: ST.Set Int, horizontal :: ST.Set Int} deriving (Show, Eq)
 
 -- 42974
 -- 0.02 secs
 part1 :: IO Int
 part1 = do
   patterns <- getPatterns "./fixtures/input13.txt"
-  return
-    . foldl (\count (Reflections v h) -> count + (sum . map (* 1) $ v) + (sum . map (* 100) $ h)) 0
-    . map getBothReflections
-    $ patterns
+  return . scoreReflections . map findOriginalRefl $ patterns
 
-getBothReflections :: Eq a => [[a]] -> Reflections
-getBothReflections xs = Reflections vertical horizontal
+-- print . length . concat . concat $ patterns
+-- gives me 16,394 so I reckon part 2 is brute forcible without much fuss
+
+-- 27587
+-- 0.19 secs
+part2 :: IO Int
+part2 = do
+  patterns <- getPatterns "./fixtures/input13.txt"
+  return . scoreReflections . map findNewRefl $ patterns
+
+scoreReflections :: [Reflections] -> Int
+scoreReflections = foldl (\count (Reflections v h) -> count + (sum . ST.map (* 1) $ v) + (sum . ST.map (* 100) $ h)) 0
+
+findNewRefl :: Pattern -> Reflections
+findNewRefl p =
+  head
+    . filter (\(Reflections v h) -> not . null $ ST.union v h)
+    . map (diffWithOldRefl originalRefls . findOriginalRefl)
+    . getAllReplacements
+    $ p
   where
-    vertical = getAllReflections . transpose $ xs
-    horizontal = getAllReflections xs
+    originalRefls = findOriginalRefl p
+
+diffWithOldRefl :: Reflections -> Reflections -> Reflections
+diffWithOldRefl (Reflections oldV oldH) (Reflections newV newH) = Reflections (ST.difference newV oldV) (ST.difference newH oldH)
+
+getAllReplacements :: Pattern -> [Pattern]
+getAllReplacements ps = map ((map toList . toList) . (`applyUpdate` asSeq)) points
+  where
+    points = [(x, y) | (y, xs) <- zip [0 ..] ps, (x, _) <- zip [0 ..] xs]
+    asSeq = S.fromList . map S.fromList $ ps
+    applyUpdate :: (Int, Int) -> S.Seq (S.Seq Char) -> S.Seq (S.Seq Char)
+    applyUpdate (x, y) = S.adjust' (S.adjust' swapChar x) y
+    swapChar :: Char -> Char
+    swapChar '#' = '.'
+    swapChar '.' = '#'
+    swapChar _ = undefined
+
+findOriginalRefl :: Eq a => [[a]] -> Reflections
+findOriginalRefl xs = Reflections vertical horizontal
+  where
+    vertical = ST.fromList . getAllReflections . transpose $ xs
+    horizontal = ST.fromList . getAllReflections $ xs
 
 getAllReflections :: Eq a => [a] -> [Int]
 getAllReflections = map fst . filter (uncurry areReflections . snd) . getAllSplits
