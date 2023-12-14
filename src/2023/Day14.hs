25d7806de2a109fa3fa3661899449b85d101f1b0
paul <pmstanford95@gmail.com>
2023-12-14 20:26:26 +0000

    cycle function working

--- src/2023/Day14.hs
+++ src/2023/Day14.hs
@@ -7,16 +7,46 @@ part1 = do
   inp <- getLines "./fixtures/input14.txt"
   return . sum . zipWith (\i r -> i * countRoundRocks r) [1 ..] . reverse . shiftNorth $ inp
 
+part2 = do
+  inp <- getLines "./fixtures/input14Toy.txt"
+  let res1 = applyCycle inp
+  let res2 = applyCycle res1
+  let res3 = applyCycle res2
+  traverse_ putStrLn res1
+  putStrLn ""
+  traverse_ putStrLn res2
+  putStrLn ""
+  traverse_ putStrLn res3
+
+applyCycle :: [String] -> [String]
+applyCycle = shiftEast . shiftSouth . shiftWest . shiftNorth
+
+shiftNorth :: [String] -> [String]
+shiftNorth = reverse . transpose . map processRow . transpose . reverse
+
+shiftSouth :: [String] -> [String]
+shiftSouth = reverse . transpose . map processRow' . transpose . reverse
+
+shiftWest :: [String] -> [String]
+shiftWest = map processRow'
+
+shiftEast :: [String] -> [String]
+shiftEast = map processRow
+
+processRow :: String -> String
+processRow = concatMap processGroup . groupBy groupFn
+  where
+    groupFn x y = (x == '#' && y == '#') || (x /= '#' && y /= '#')
+    processGroup g = if head g == '#' then g else sort g
+
+processRow' :: String -> String
+processRow' = concatMap processGroup . groupBy groupFn
+  where
+    groupFn x y = (x == '#' && y == '#') || (x /= '#' && y /= '#')
+    processGroup g = if head g == '#' then g else reverse . sort $ g
+
 countRoundRocks :: [Char] -> Int
 countRoundRocks = length . filter (== 'O')
 
 getLines :: FilePath -> IO [String]
 getLines filePath = lines <$> readFile filePath
-
-shiftNorth :: [String] -> [String]
-shiftNorth = reverse . transpose . map processRow . transpose . reverse
-
-processRow :: String -> String
-processRow = concatMap processGroup . groupBy (\x y -> (x == '#' && y == '#') || (x /= '#' && y /= '#'))
-  where
-    processGroup g = if head g == '#' then g else sort g
