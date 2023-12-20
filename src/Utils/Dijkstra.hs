a261a0df8b5416201b082b3b621b876aa77d9273
paul <pmstanford95@gmail.com>
2023-12-20 12:12:22 +0000

    split map in half

--- src/Utils/Dijkstra.hs
+++ src/Utils/Dijkstra.hs
@@ -1,13 +1,15 @@
-module Utils.Dijkstra (dijkstra, StartNode, EndNode) where
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Utils.Dijkstra (dijkstra, StartNode, EndNode, DijkstraState) where
 
 -- based on my day 12 2022 implementation
 -- assuming score is always an int but can probs generalise
 -- also should definitely generalise using Coords to just be some note type!
 
-import Data.Bifunctor (second)
 import Data.List.Extra (find, minimumOn)
-import Data.Map qualified as M (Map, adjust, alter, filter, filterWithKey, lookup, singleton, toList)
-import Data.Set qualified as S (Set, map)
+import Data.Map qualified as M (Map, alter, delete, empty, filterWithKey, insert, lookup, notMember, singleton, toList)
+import Data.Maybe (isJust)
+import Data.Set qualified as S (Set, map, member)
 
 type StartNode nodeId = nodeId
 
@@ -15,10 +17,21 @@ type EndNode nodeId = nodeId
 
 data Visitation = Visited | UnVisited deriving (Show, Eq, Ord)
 
-type TentativeDistances nodeId = M.Map nodeId (Int, Visitation)
+data DijkstraState nodeId = DState
+  { visited :: FinalisedDistances nodeId,
+    unVisited :: TentativeDistances nodeId,
+    isFinished :: Bool
+  }
+  deriving (Show)
 
-shouldStop :: Ord nodeId => S.Set (EndNode nodeId) -> TentativeDistances nodeId -> Bool
-shouldStop endNodeCoords tds = any (\x -> fmap snd x == Just Visited) . S.map (`M.lookup` tds) $ endNodeCoords
+type TentativeDistances nodeId = M.Map nodeId Int
+
+type FinalisedDistances nodeId = M.Map nodeId Int
+
+-- will become a simple element check I think
+-- I think this could be more efficient if I had the lookup happen each time we insert node into map
+shouldStop :: Ord nodeId => S.Set (EndNode nodeId) -> DijkstraState nodeId -> Bool
+shouldStop endNodeCoords ds = any isJust . S.map (\x -> M.lookup x (visited ds)) $ endNodeCoords
 
 dijkstra ::
   Ord nodeId =>
@@ -26,27 +39,29 @@ dijkstra ::
   (nodeId -> [nodeId]) -> -- neighbourGetter
   S.Set (EndNode nodeId) ->
   StartNode nodeId ->
-  Maybe (TentativeDistances nodeId)
+  Maybe (FinalisedDistances nodeId)
 dijkstra scoreFn neighbourGetter endNodes startNode =
-  fmap (M.filterWithKey (\k (_, v) -> k `elem` endNodes && v == Visited))
+  fmap (M.filterWithKey (\k _ -> k `elem` endNodes) . visited)
     . find (shouldStop endNodes)
-    . iterate (dijkstraStep scoreFn neighbourGetter)
+    . iterate (dijkstraStep scoreFn neighbourGetter endNodes)
     $ dInit
   where
-    dInit = M.singleton startNode (0, UnVisited)
+    dInit = DState M.empty (M.singleton startNode 0) False
 
 dijkstraStep ::
   Ord nodeId =>
   (nodeId -> Int) ->
   (nodeId -> [nodeId]) ->
-  TentativeDistances nodeId ->
-  TentativeDistances nodeId
-dijkstraStep scoreFn neighbourGetter distanceMap =
-  let (currentNodeCoords, currentNodeDist) = getCurrentNode distanceMap
-      neighbours = neighbourGetter currentNodeCoords
-      res = updateNeighbours scoreFn currentNodeDist distanceMap neighbours
-      res' = M.adjust (\(i, _) -> (i, Visited)) currentNodeCoords res
-   in res'
+  S.Set (EndNode nodeId) ->
+  DijkstraState nodeId ->
+  DijkstraState nodeId
+dijkstraStep scoreFn neighbourGetter endNodes (DState visited unVisited _) = DState visited' unVisited' isFinished'
+  where
+    (currentNodeCoords, currentNodeDist) = getCurrentNode unVisited
+    neighbours = filter (`M.notMember` visited) . neighbourGetter $ currentNodeCoords
+    unVisited' = M.delete currentNodeCoords . updateNeighbours scoreFn currentNodeDist unVisited $ neighbours
+    visited' = M.insert currentNodeCoords currentNodeDist visited
+    isFinished' = S.member currentNodeCoords endNodes
 
 updateNeighbours ::
   Ord nodeId =>
@@ -70,19 +85,14 @@ updateNeighbour scoreFn currentNodeDistance distanceMap neighbor =
     newScore :: Int
     newScore = currentNodeDistance + scoreFn neighbor
 
-    alterFn :: Maybe (Int, Visitation) -> (Int, Visitation)
-    alterFn (Just (i, v)) = (min i newScore, v)
-    alterFn Nothing = (newScore, UnVisited)
+    alterFn :: Maybe Int -> Int
+    alterFn (Just i) = min i newScore
+    alterFn Nothing = newScore
 
     -- like alter but can't delete elements
     alter' :: Ord k => (Maybe a -> a) -> k -> M.Map k a -> M.Map k a
     alter' f = M.alter (Just . f)
 
 -- I think really this should be more like a queue than having to iterate through a map each time
--- at the very least I could split up the Visited and UnVisited into separate maps...
 getCurrentNode :: TentativeDistances nodeId -> (nodeId, Int)
-getCurrentNode =
-  minimumOn snd
-    . map (second fst)
-    . M.toList
-    . M.filter (\(_, v) -> v == UnVisited)
+getCurrentNode = minimumOn snd . M.toList
