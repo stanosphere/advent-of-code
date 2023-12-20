328187ebcec115399fc8b979bc73f397788ed2c5
paul <pmstanford95@gmail.com>
2023-12-20 20:25:02 +0000

    part 1 day 19 was worryingly easy

--- src/2023/Day19.hs
+++ src/2023/Day19.hs
@@ -2,27 +2,62 @@
 
 module Day19 where
 
-import Data.Map qualified as M (Map, fromList)
+import Data.Foldable ()
+import Data.List.Extra (splitOn)
+import Data.Map qualified as M (Map, elems, fromList, (!))
+import Data.Maybe (mapMaybe)
 import Text.Parsec qualified as P
 import Text.ParserCombinators.Parsec (Parser, parse, (<|>))
 
 type Part = M.Map Char Int
 
-data Destination = Accept | Reject | ToWorkFlow String deriving (Show)
+data Destination = Accept | Reject | ToWorkFlow String deriving (Show, Eq)
 
 data Rule = Rule {condition :: Condition, destination :: Destination} deriving (Show)
 
-data WorkFlow = WF {id :: String, rules :: [Rule]} deriving (Show)
+data WorkFlow = WF {wfId :: String, rules :: [Rule]} deriving (Show)
 
 data Op = GreaterThan | LessThan deriving (Show)
 
 data Condition = Cond {partType :: Char, op :: Op, comparator :: Int} | Default deriving (Show)
 
--- need function that maps a condtioon to a function..
+-- need function that maps a condition to a function...
 
-part1 = unsafeParse partParser "{x=2036,m=264,a=79,s=2244}"
+part1 = do
+  rawInput <- getLines "./fixtures/input19.txt"
+  let [rawWorkflows, rawParts] = splitOn [""] rawInput
+  let workflows = M.fromList . map ((\wf -> (wfId wf, wf)) . unsafeParse workFlowParser) $ rawWorkflows
+  let parts = map (unsafeParse partParser) rawParts
+  let res = sum . map sumRatingNumbers . filter ((== Accept) . applyWorkFlows workflows) $ parts
 
--- lots of parsing for thi one!
+  print res
+
+-- start in workflow named in
+
+sumRatingNumbers :: Part -> Int
+sumRatingNumbers = sum . M.elems
+
+applyWorkFlows :: M.Map String WorkFlow -> Part -> Destination
+applyWorkFlows wfs p = applyWorkFlows' (ToWorkFlow "in")
+  where
+    applyWorkFlows' :: Destination -> Destination
+    applyWorkFlows' (ToWorkFlow d) = applyWorkFlows' (applyWorkFlow p (wfs M.! d))
+    applyWorkFlows' dest = dest
+
+applyWorkFlow :: Part -> WorkFlow -> Destination
+applyWorkFlow part (WF _ rules) = head . mapMaybe applyRule $ rules
+  where
+    applyRule (Rule condition destination) =
+      if conditionToFunction condition part
+        then Just destination
+        else Nothing
+
+conditionToFunction :: Condition -> (Part -> Bool)
+conditionToFunction Default _ = True
+conditionToFunction (Cond partType GreaterThan comparator) p = (p M.! partType) > comparator
+conditionToFunction (Cond partType LessThan comparator) p = (p M.! partType) < comparator
+
+-- lots of parsing for this one!
 unsafeParse :: Parser a -> String -> a
 unsafeParse p s = case parse p "whatever" s of
   Left res -> error . show $ res
@@ -33,10 +68,11 @@ workFlowParser = do
   wId <- P.many1 P.letter
   _ <- P.char '{'
   rules <- P.sepBy ruleParser (P.char ',')
+  _ <- P.char '}'
   return (WF wId rules)
 
 ruleParser :: Parser Rule
-ruleParser = conditionedRuleParser <|> conditionedRuleParser
+ruleParser = P.try conditionedRuleParser <|> defaultRuleParser
 
 defaultRuleParser :: Parser Rule
 defaultRuleParser = Rule Default <$> destinationParser
@@ -65,7 +101,7 @@ destinationParser :: Parser Destination
 destinationParser =
   (Accept <$ P.char 'A')
     <|> (Reject <$ P.char 'R')
-    <|> (ToWorkFlow <$> P.many1 (P.noneOf "RA"))
+    <|> (ToWorkFlow <$> P.many1 P.letter)
 
 -- {x=2036,m=264,a=79,s=2244}
 partParser :: Parser Part
@@ -77,3 +113,6 @@ tupleParser = do
   _ <- P.char '='
   partRating <- intParser
   return (partId, partRating)
+
+getLines :: FilePath -> IO [String]
+getLines filePath = fmap lines (readFile filePath)
