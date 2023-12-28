8ac0640bf6add8bc64a0c944fb680be260b36172
paul <pmstanford95@gmail.com>
2023-12-28 18:09:21 +0000

    naive method working for junction graph on toy input

--- src/2023/Day23Part2.hs
+++ src/2023/Day23Part2.hs
@@ -1,6 +1,6 @@
 module Day23Part2 where
 
-import Data.Foldable (traverse_)
+import Data.Foldable (find, traverse_)
 import Data.Map qualified as M
 import Data.Set qualified as S
 import Utils.Grouping (groupBy', groupMap)
@@ -9,9 +9,7 @@ type Coords = (Int, Int)
 
 type Grid = S.Set Coords
 
-type Graph = M.Map Coords [Coords]
-
-type ScoreMap = M.Map (Coords, Coords) Int
+type Graph = M.Map Coords [(Coords, Int)]
 
 data Edge = Edge {from :: Coords, to :: Coords, score :: Int} deriving (Show)
 
@@ -24,45 +22,41 @@ part2 = do
   grid <- getGrid <$> getLines "./fixtures/input23Toy.txt"
   let startNode :: Coords = (1, 0)
   let endNode :: Coords = (21, 22) -- real end is (139, 140)
-  let (graph, scoreMap) = buildGraph grid startNode endNode
+  let graph = buildGraph grid startNode endNode
+  let res = solve graph endNode
+  print res
 
-  traverse_ print graph
-  traverse_ print scoreMap
-
-buildGraph :: Grid -> Coords -> Coords -> (Graph, ScoreMap)
-buildGraph grid startNode endNode =
-  let allPoints = S.toList grid
-      junctions = filter (isJunction grid) allPoints
+buildGraph :: Grid -> Coords -> Coords -> Graph
+buildGraph g startNode endNode =
+  let allPoints = S.toList g
+      junctions = filter isJunction allPoints
       nodes = S.fromList (startNode : endNode : junctions)
-      edges = findEdges grid nodes
-      graph = groupMap from to edges
-      scoreMap = M.map (\x -> if length x == 1 then score . head $ x else undefined) . groupBy' (\x -> (from x, to x)) $ edges
-   in (graph, scoreMap)
-
--- remember to pass in start and end nodes...
-findEdges :: Grid -> S.Set Coords -> [Edge]
-findEdges g nodes = concatMap (\n -> findEdgesForNode g n (others n)) . S.toList $ nodes
+      edges = findEdges nodes
+      graph = groupMap from (\e -> (to e, score e)) edges
+   in graph
   where
-    others n = S.delete n nodes
+    isJunction :: Coords -> Bool
+    isJunction = (>= 3) . length . getNeighbours
 
-findEdgesForNode :: Grid -> Coords -> S.Set Coords -> [Edge]
-findEdgesForNode g c otherNodes =
-  map (uncurry (Edge c) . (\n -> followPathToEdge g otherNodes (c, n, 0)))
-    . getNeighbours g
-    $ c
+    findEdges :: S.Set Coords -> [Edge]
+    findEdges nodes = concatMap (\n -> findEdgesForNode n (S.delete n nodes)) . S.toList $ nodes
 
-followPathToEdge :: Grid -> S.Set Coords -> (Coords, Coords, Int) -> (Coords, Int)
-followPathToEdge g otherNodes (prev, current, i) =
-  if S.member nextNode otherNodes
-    then (nextNode, i' + 1)
-    else followPathToEdge g otherNodes (current, nextNode, i')
-  where
-    nextNode = head . filter (/= prev) . getNeighbours g $ current
-    i' = i + 1
+    findEdgesForNode :: Coords -> S.Set Coords -> [Edge]
+    findEdgesForNode c otherNodes =
+      map (uncurry (Edge c) . (\n -> followPathToEdge otherNodes (c, n, 0)))
+        . getNeighbours
+        $ c
 
--- could use intersections and diffs I suppose
-getNeighbours :: Grid -> Coords -> [Coords]
-getNeighbours g (x, y) = filter (`S.member` g) [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    followPathToEdge :: S.Set Coords -> (Coords, Coords, Int) -> (Coords, Int)
+    followPathToEdge otherNodes (prev, current, i) =
+      let nextNode = head . filter (/= prev) . getNeighbours $ current
+          i' = i + 1
+       in if S.member nextNode otherNodes
+            then (nextNode, i' + 1)
+            else followPathToEdge otherNodes (current, nextNode, i')
+
+    getNeighbours :: Coords -> [Coords]
+    getNeighbours (x, y) = filter (`S.member` g) [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 getGrid :: [String] -> Grid
 getGrid inp = S.fromList [(x, y) | (y, xs) <- zip [0 ..] inp, (x, c) <- zip [0 ..] xs, c /= '#']
@@ -70,10 +64,31 @@ getGrid inp = S.fromList [(x, y) | (y, xs) <- zip [0 ..] inp, (x, c) <- zip [0 .
 getLines :: FilePath -> IO [String]
 getLines filePath = fmap lines (readFile filePath)
 
-isJunction :: Grid -> Coords -> Bool
-isJunction g = (>= 3) . length . getNeighbours g
+data Path = Path {current :: Coords, rest :: S.Set Coords, size :: Int} deriving (Show)
 
-prettyPrintSymbolMap :: Int -> [Coords] -> IO ()
-prettyPrintSymbolMap size mp =
-  let counter = [0 .. size]
-   in traverse_ putStrLn [[if (x, y) `elem` mp then '0' else ' ' | x <- counter] | y <- counter]
+data State = State {unfinished :: [Path], finished :: [Path]} deriving (Show)
+
+-- -- solve :: Graph -> Coords -> Maybe Int
+-- solve grid endNode =
+--   iterate (step grid endNode) $ initState
+--   where
+--     initState = State [Path (1, 0) S.empty 0] []
+
+solve :: Graph -> Coords -> Maybe Int
+solve grid endNode =
+  fmap (maximum . map (size) . finished) . find (null . unfinished) . iterate (step grid endNode) $ initState
+  where
+    initState = State [Path (1, 0) S.empty 0] []
+
+step :: Graph -> Coords -> State -> State
+step g endCoords (State unfinished finished) =
+  let stepped = concatMap (stepPath g) unfinished
+      newFinished = filter (\p -> current p == endCoords) stepped
+      newUnfinished = filter (\p -> current p /= endCoords) stepped
+   in State newUnfinished (finished ++ newFinished)
+
+stepPath :: Graph -> Path -> [Path]
+stepPath g p = map (appendPath p) . filter (\(c, _) -> S.notMember c (rest p)) . (g M.!) . current $ p
+
+appendPath :: Path -> (Coords, Int) -> Path
+appendPath (Path prev rest len) (c, i) = Path c (S.insert prev rest) (len + i)
