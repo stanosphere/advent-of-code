46650c805699f9e52a27fb1c644f9c76ef16199b
paul <pmstanford95@gmail.com>
2023-12-30 17:40:16 +0000

    more thoughts about min cut algos

--- src/2023/Day25.hs
+++ src/2023/Day25.hs
@@ -15,6 +15,17 @@ import Data.List.Extra (nub, splitOn)
 -- 3317 C 3 is 6,077,042,530
 -- and I don't think doing 6 billion iterations is a good idea...
 
+-- looks like Karger's algorithm will do this for us
+-- https://en.wikipedia.org/wiki/Karger%27s_algorithm
+-- but it requires random numbers which I've actually never used in Haskell
+-- I'll also consult my algorithms book...
+-- there are mentions of cuts in the Ford-Fulkerson section but that's more about network flows and such
+-- and indeed because it's about flows that's all about directed graphs which isn't what we're dealing with here
+-- Cuts are also mentioned in the minimum spanning tree section of my book
+-- but I don't think that's useful for this problem
+-- there's also the Stoer Wagner algorithm which might be a little easier to implement if less efficient than Karger's
+-- https://en.wikipedia.org/wiki/Stoer%E2%80%93Wagner_algorithm
+-- I can't see it mentioned in the book
 part1 = do
   inp <- getLines "./fixtures/input25.txt"
   let graph = makeGraph inp
