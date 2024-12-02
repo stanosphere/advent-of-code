25db939bb2e99aa1aa175f3eff27f78d73e541d6
paul <pmstanford95@gmail.com>
2024-12-02 13:10:39 +0000

    day 2 part 2 but its not like the best solution by any means

--- src/2024/Day2.hs
+++ src/2024/Day2.hs
@@ -1,12 +1,24 @@
 module Day2 where
 
-import Data.List (tails)
+import Data.List (inits, tails)
 
 part1 :: IO Int
-part1 = solvePart1 <$> getInput "./fixtures/input2.txt"
+part1 = length . filter isSafe <$> getInput
 
-solvePart1 :: [[Int]] -> Int
-solvePart1 = length . filter isSafe
+part2 :: IO Int
+part2 = length . filter isSafe' <$> getInput
+
+-- same as isSafe but can tolerate a single bad level
+-- something _might_ be safe if
+-- - there is exactly one bad difference
+-- - there is exactly two bad differences and they are next to each other
+-- however just to solve the problem I think the easiest thing will be to just remove each element from a bad list and see what happens...
+-- still runs in like 0.04 secs so it's calm
+isSafe' :: [Int] -> Bool
+isSafe' xs = isSafe xs || (any isSafe . withOneElementRemoved $ xs)
+
+withOneElementRemoved :: [a] -> [[a]]
+withOneElementRemoved xs = zipWith (++) (inits xs) (drop 1 . tails $ xs)
 
 -- The levels are either all increasing or all decreasing.
 -- Any two adjacent levels differ by at least one and at most three.
@@ -22,5 +34,5 @@ pairs = map (\[x, y] -> (x, y)) . windows 2
 windows :: Int -> [a] -> [[a]]
 windows n = takeWhile ((n ==) . length) . map (take n) . tails
 
-getInput :: FilePath -> IO [[Int]]
-getInput filePath = map (map read . words) . lines <$> readFile filePath
+getInput :: IO [[Int]]
+getInput = map (map read . words) . lines <$> readFile "./fixtures/input2.txt"
