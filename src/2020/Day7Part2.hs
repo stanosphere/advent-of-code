e4cbbf145b500281d32d001eedcab277adcf64ea
paul <pmstanford95@gmail.com>
2024-12-07 22:52:20 +0000

    don't use qualified imports for Parsec

--- src/2020/Day7Part2.hs
+++ src/2020/Day7Part2.hs
@@ -4,7 +4,18 @@ import Data.Functor (($>))
 import qualified Data.Map as M
 import Data.Maybe (catMaybes)
 import Data.Tree (Tree, foldTree, unfoldTree)
-import qualified Text.Parsec as P
+import Text.Parsec
+  ( char,
+    digit,
+    letter,
+    many1,
+    manyTill,
+    parse,
+    sepBy,
+    string,
+    try,
+    (<|>),
+  )
 import Text.ParserCombinators.Parsec (Parser, parse)
 
 -- hmm I did part 1 like agesssss ago
@@ -18,7 +29,7 @@ type RuleMap = M.Map BagType [(BagType, Int)]
 
 -- have to subtract off the 1 shiny gold back at the end since we care only about the bags _inside_ it
 part2 :: IO Int
-part2 = (+ (-1)) . sumTree . buildTree <$> getInput
+part2 = (+ (-1)) . sumTree . buildTree <$> getPuzzleInput
 
 sumTree :: Tree (BagType, Int) -> Int
 sumTree = foldTree (\(_, x) xs -> x * (1 + sum xs))
@@ -29,8 +40,8 @@ buildTree ruleMap = unfoldTree getChildren ("shiny gold", 1)
     getChildren :: (BagType, Int) -> ((BagType, Int), [(BagType, Int)])
     getChildren (nodeLabel, n) = ((nodeLabel, n), ruleMap M.! nodeLabel)
 
-getInput :: IO RuleMap
-getInput = toRuleMap . map (unsafeParse ruleParser) . lines <$> readFile "./fixtures/input7.txt"
+getPuzzleInput :: IO RuleMap
+getPuzzleInput = toRuleMap . map (unsafeParse ruleParser) . lines <$> readFile "./fixtures/input7.txt"
 
 toRuleMap :: [Rule] -> RuleMap
 toRuleMap = M.fromList . map (\r -> (_outer r, _inner r))
@@ -42,29 +53,29 @@ unsafeParse p s = case parse p "" s of
 
 ruleParser :: Parser Rule
 ruleParser = do
-  outer <- P.manyTill bagTypeCharParser (P.try . P.string $ " bags contain ")
-  inners <- catMaybes <$> P.sepBy bagParser (P.string ", ")
-  _ <- P.char '.'
+  outer <- manyTill bagTypeCharParser (try . string $ " bags contain ")
+  inners <- catMaybes <$> sepBy bagParser (string ", ")
+  _ <- char '.'
   return (Rule outer inners)
   where
     bagParser :: Parser (Maybe (BagType, Int))
-    bagParser = P.try nonEmptyBagParser P.<|> emptyBagParser
+    bagParser = try nonEmptyBagParser <|> emptyBagParser
 
     emptyBagParser :: Parser (Maybe (BagType, Int))
-    emptyBagParser = P.string "no other bags" $> Nothing
+    emptyBagParser = string "no other bags" $> Nothing
 
     nonEmptyBagParser :: Parser (Maybe (BagType, Int))
     nonEmptyBagParser = do
       n <- intParser
-      _ <- P.char ' '
-      bagType <- P.manyTill bagTypeCharParser bagStatementEndParser
+      _ <- char ' '
+      bagType <- manyTill bagTypeCharParser bagStatementEndParser
       return . Just $ (bagType, n)
       where
-        -- annoyingly if there's one bag it ends in "bag rather than bags!"
-        bagStatementEndParser = (P.try . P.string $ " bags") P.<|> (P.try . P.string $ " bag")
+        -- annoyingly if there's one bag it ends in "bag" rather than "bags"!
+        bagStatementEndParser = (try . string $ " bags") <|> (try . string $ " bag")
 
     bagTypeCharParser :: Parser Char
-    bagTypeCharParser = P.letter P.<|> P.char ' '
+    bagTypeCharParser = letter <|> char ' '
 
     intParser :: Parser Int
-    intParser = read <$> P.many1 P.digit
+    intParser = read <$> many1 digit
