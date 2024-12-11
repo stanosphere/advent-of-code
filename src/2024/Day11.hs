de25cfa0c73ca194694ecdf7c291c091ab505ad7
paul <pmstanford95@gmail.com>
2024-12-11 15:03:50 +0000

    day 11 part 2

--- src/2024/Day11.hs
+++ src/2024/Day11.hs
@@ -1,15 +1,32 @@
 module Day11 where
 
 import Data.List.Split (splitOn)
+import qualified Data.Map as M
+
+-- apparently the official name is Multiset https://en.wikipedia.org/wiki/Multiset
+type Bag = M.Map Int Int
 
 part1 :: IO Int
 part1 = length . (!! 25) . iterate step <$> getInput
 
 part2 :: IO Int
-part2 = length . (!! 75) . iterate step <$> getInput
+part2 = totalStones . (!! 75) . iterate step' . frequencies <$> getInput
+
+totalStones :: Bag -> Int
+totalStones = M.foldr (+) 0
 
 step :: [Int] -> [Int]
 step = concatMap transformStone
+
+-- not sure if this is nested enough lol
+step' :: Bag -> Bag
+step' = M.foldrWithKey folder M.empty
+  where
+    folder :: Int -> Int -> Bag -> Bag
+    folder stoneValue stoneCount bag = foldr insert bag newValues
+      where
+        newValues = transformStone stoneValue
+        insert newValue = M.insertWith (+) newValue stoneCount
 
 transformStone :: Int -> [Int]
 transformStone x
@@ -24,3 +41,9 @@ splitInHalf xs = case splitAt splitIndex xs of (l, r) -> [l, r]
 
 getInput :: IO [Int]
 getInput = map read . splitOn " " <$> readFile "./fixtures/input11.txt"
+
+-- taken from Day 1 and Day 10 of this year, might be worth popping it in some util module now that it's used 3 times
+frequencies :: (Ord a) => [a] -> M.Map a Int
+frequencies = foldr incrementMap M.empty
+  where
+    incrementMap x = M.insertWith (+) x 1
