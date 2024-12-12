8947fcbe4763e4a992fd0f43b6ddb62248163744
paul <pmstanford95@gmail.com>
2024-12-12 17:50:05 +0000

    attmept part 2 but it fails the nasty mobius example

--- src/2024/Day12.hs
+++ src/2024/Day12.hs
@@ -1,9 +1,10 @@
 module Day12 where
 
+import Data.List (groupBy, sort)
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import qualified Data.Set as S
-import Utils.Grouping (frequencies)
+import Utils.Grouping (frequencies, groupMap)
 import qualified Utils.UnionFind as UF (clusterMap, fromList, unionClusters)
 
 -- hmmmmm maybe union find would be the way...
@@ -20,6 +21,54 @@ type Clusters = [[Coord]]
 
 type Edge = (Coord, Coord)
 
+-- get edge count
+-- so I think we can find those sub-edges which lie on the perimeter using something similar to getPerimeter
+-- given these edges we can group them by x coord and sort each group by the y coord (or indeed go the other way around)
+-- and then each continuous sub list constitutes an edge
+-- I think this will work with shapes with holes in and all
+-- as a sense check I could also try the same technique and switch x and y
+
+getEdgeCount :: [Coord] -> Int
+getEdgeCount xs = (sum . map countContiguousRegions $ horizontalEdges) + (sum . map countContiguousRegions $ verticalEdges)
+  where
+    edgesOnPerimeter = M.keys . M.filter (== 1) . frequencies . concatMap toEdges $ xs
+    horizontalEdges =
+      M.elems
+        . M.map sort
+        . groupMap (\((_, y1), (_, _)) -> y1) (\((x1, _), (_, _)) -> x1)
+        . filter isHorizontal
+        $ edgesOnPerimeter
+    verticalEdges =
+      M.elems
+        . M.map sort
+        . groupMap (\((x1, _), (_, _)) -> x1) (\((_, y1), (_, _)) -> y1)
+        . filter isVertical
+        $ edgesOnPerimeter
+
+    isHorizontal :: Edge -> Bool
+    isHorizontal ((_, y1), (_, y2)) = y1 == y2
+
+    isVertical :: Edge -> Bool
+    isVertical ((x1, _), (x2, _)) = x1 == x2
+
+    toEdges :: Coord -> [Edge]
+    toEdges (x, y) =
+      [ ((x, y), (x + 1, y)),
+        ((x, y), (x, y + 1)),
+        ((x + 1, y), (x + 1, y + 1)),
+        ((x, y + 1), (x + 1, y + 1))
+      ]
+
+    countContiguousRegions :: [Int] -> Int
+    countContiguousRegions = length . getContiguousRegions
+
+    getContiguousRegions :: [Int] -> [[Int]]
+    getContiguousRegions = foldr folder []
+      where
+        folder :: Int -> [[Int]] -> [[Int]]
+        folder x [] = [[x]]
+        folder x ((y : ys) : yss) = if x + 1 == y then (x : y : ys) : yss else [x] : (y : ys) : yss
+
 part1 :: IO Int
 part1 =
   sum
@@ -28,6 +77,21 @@ part1 =
     . parseInput
     . lines
     <$> readFile "./fixtures/input12.txt"
+
+part2 :: IO Int
+part2 =
+  sum
+    . map getClusterScore'
+    . toClusters
+    . parseInput
+    . lines
+    <$> readFile "./fixtures/input12.txt"
+
+getClusterScore' :: [Coord] -> Int
+getClusterScore' xs = getEdgeCount xs * getArea xs
+  where
+    getArea :: [Coord] -> Int
+    getArea = length
 
 getClusterScore :: [Coord] -> Int
 getClusterScore xs = getPerimeter xs * getArea xs
