a493cadaae34f95f6bb5d09d8c21267ed1c00eb8
paul <pmstanford95@gmail.com>
2024-12-13 14:48:27 +0000

    and thats it for day 13

--- src/2024/Day13.hs
+++ src/2024/Day13.hs
@@ -1,8 +1,9 @@
 module Day13 where
 
+import Data.Bifunctor (bimap)
 import Data.Maybe (mapMaybe)
 import qualified Text.Parsec as P
-import Text.ParserCombinators.Parsec (Parser, parse, try)
+import Text.ParserCombinators.Parsec (Parser, parse)
 
 -- it costs 3 tokens to push the A button
 -- and 1 token to push the B button
@@ -25,17 +26,32 @@ import Text.ParserCombinators.Parsec (Parser, parse, try)
 -- a = (x2 z1 - x1 z2) / (y1 x2 - y2 x1)
 -- b = (y1 z2 - y2 z1) / (y1 x2 - y2 x1)
 
-data Equation = Eqn {z :: Int, y :: Int, x :: Int} deriving (Show)
+data Equation = Eqn {_z :: Int, _y :: Int, _x :: Int} deriving (Show)
 
 data Solution = Sol {_a :: Int, _b :: Int} deriving (Show)
 
-part1 = sum . map (\(Sol a b) -> 3 * a + b) . mapMaybe (uncurry solveEqns) <$> getInput
+part1 :: IO Int
+part1 =
+  sum
+    . map (\(Sol a b) -> 3 * a + b)
+    . mapMaybe (uncurry solveEqns)
+    <$> getInput
+
+part2 :: IO Int
+part2 =
+  sum
+    . map (\(Sol a b) -> 3 * a + b)
+    . mapMaybe (uncurry solveEqns . bimap addBigOffset addBigOffset)
+    <$> getInput
+
+addBigOffset :: Equation -> Equation
+addBigOffset (Eqn z y x) = Eqn (10000000000000 + z) y x
 
 solveEqns :: Equation -> Equation -> Maybe Solution
 solveEqns (Eqn z1 y1 x1) (Eqn z2 y2 x2)
   | denom == 0 = Nothing
   | aNumerator `mod` denom /= 0 = Nothing
-  | aNumerator `mod` denom /= 0 = Nothing
+  | bNumerator `mod` denom /= 0 = Nothing
   | otherwise = Just (Sol (aNumerator `div` denom) (bNumerator `div` denom))
   where
     denom = y1 * x2 - y2 * x1
