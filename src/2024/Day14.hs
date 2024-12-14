1b0a79b0088ca4ca98ad2da8b2a8127f731bc284
paul <pmstanford95@gmail.com>
2024-12-14 13:27:51 +0000

    solve day 14 part 2

--- src/2024/Day14.hs
+++ src/2024/Day14.hs
@@ -1,7 +1,9 @@
 module Day14 where
 
+import Data.Foldable (traverse_)
 import qualified Text.Parsec as P
 import Text.ParserCombinators.Parsec (Parser, parse)
+import Utils.Grouping (windows)
 
 type Coord = (Int, Int)
 
@@ -10,19 +12,38 @@ data Robot = Robot {_position :: Coord, _velocity :: Coord} deriving (Show)
 data GridSize = GS {_width :: Int, _height :: Int}
 
 part1 :: IO Int
-part1 = solve (GS 101 103) <$> getInput
+part1 = solve 100 (GS 101 103) <$> getInput
 
-solve :: GridSize -> [Robot] -> Int
-solve (GS width height) robots = product [q1, q2, q3, q4]
+part2 :: IO ()
+part2 = do
+  robots <- getInput
+  let gridSize = GS 101 103
+  traverse_ (f . (\steps -> (steps, toDisplay gridSize . map (getRobotFinalPosition steps gridSize) $ robots))) [1 .. 10000]
+
+f :: (Int, Maybe [String]) -> IO ()
+f (x, y) = case y of
+  Nothing -> print (show x ++ " (not making file though)")
+  Just fileLines -> print x *> writeFile ("./out/file" ++ show x ++ ".txt") (unlines fileLines)
+
+toDisplay :: GridSize -> [Coord] -> Maybe [String]
+toDisplay (GS width height) robots = if shouldDisplay then Just display else Nothing
   where
-    finalPositions = map (getRobotFinalPosition (GS width height)) robots
+    display = [[if (x, y) `elem` robots then '#' else ' ' | x <- [0 .. width - 1]] | y <- [0 .. height - 1]]
+    -- idea is that xmas tree probably has some robots in a nice line
+    shouldDisplay = any (\line -> "#####" `elem` windows 5 line) display
+
+solve :: Int -> GridSize -> [Robot] -> Int
+solve steps (GS width height) robots = product [q1, q2, q3, q4]
+  where
+    finalPositions = map (getRobotFinalPosition steps (GS width height)) robots
     q1 = length . filter (\(x, y) -> x < (width `div` 2) && y < (height `div` 2)) $ finalPositions
     q2 = length . filter (\(x, y) -> x < (width `div` 2) && y > (height `div` 2)) $ finalPositions
     q3 = length . filter (\(x, y) -> x > (width `div` 2) && y < (height `div` 2)) $ finalPositions
     q4 = length . filter (\(x, y) -> x > (width `div` 2) && y > (height `div` 2)) $ finalPositions
 
-getRobotFinalPosition :: GridSize -> Robot -> Coord
-getRobotFinalPosition (GS width height) (Robot (x, y) (vx, vy)) = ((x + vx * 100) `mod` width, (y + vy * 100) `mod` height)
+getRobotFinalPosition :: Int -> GridSize -> Robot -> Coord
+getRobotFinalPosition steps (GS width height) (Robot (x, y) (vx, vy)) =
+  ((x + vx * steps) `mod` width, (y + vy * steps) `mod` height)
 
 getInput :: IO [Robot]
 getInput = unsafeParse inputParser <$> readFile "./fixtures/input14.txt"
