c54e8d6f90f407bb9dc350d6880545c18ba863e0
paul <pmstanford95@gmail.com>
2024-12-18 23:12:50 +0000

    dy 15 2021 part 2

--- src/2021/Day15.hs
+++ src/2021/Day15.hs
@@ -1,7 +1,8 @@
 module Day15 where
 
-import Data.Char (digitToInt)
-import qualified Data.Map as M (Map, fromList, lookup)
+import Data.Char (digitToInt, intToDigit)
+import Data.Foldable (traverse_)
+import qualified Data.Map as M (Map, fromList, lookup, (!))
 import Data.Maybe (mapMaybe)
 import Utils.Dijkstra (DijkstraResult, dijkstra)
 
@@ -13,10 +14,16 @@ type AccumulatedRisk = Int
 
 type Grid = M.Map Coord Risk
 
-data GridSize = GS {_maxX :: Int, _maxY :: Int}
+data GridSize = GS {_maxX :: Int, _maxY :: Int} deriving (Show)
 
 part1 :: IO (DijkstraResult Coord Risk)
-part1 = uncurry solve <$> getInput
+part1 = uncurry solve <$> getInput parseGrid
+
+part2 :: IO (DijkstraResult Coord Risk)
+part2 = uncurry solve <$> getInput parseGrid'
+
+display :: (Grid, GridSize) -> IO ()
+display (grid, GS maxX maxY) = traverse_ putStrLn [[intToDigit (grid M.! (x, y)) | x <- [0 .. maxX]] | y <- [0 .. maxY]]
 
 solve :: Grid -> GridSize -> DijkstraResult Coord Risk
 solve grid (GS maxX maxY) = dijkstra neighbourGetter isEndNode startNode
@@ -39,8 +46,36 @@ lookup' key mp = case M.lookup key mp of
   Nothing -> Nothing
   Just value -> Just (key, value)
 
-getInput :: IO (Grid, GridSize)
-getInput = parseGrid . lines <$> readFile "./fixtures/input15.txt"
+getInput :: ([[Char]] -> (Grid, GridSize)) -> IO (Grid, GridSize)
+getInput parser = parser . lines <$> readFile "./fixtures/input15.txt"
+
+parseGrid' :: [[Char]] -> (Grid, GridSize)
+parseGrid' input = (M.fromList gridAsList, GS maxX maxY)
+  where
+    gridAsList =
+      enlargeMap
+        [ ((i, j), digitToInt char)
+          | (j, row) <- zip [0 ..] input,
+            (i, char) <- zip [0 ..] row
+        ]
+    maxX = maximum . map (fst . fst) $ gridAsList
+    maxY = maximum . map (snd . fst) $ gridAsList
+
+    enlargeMap = concatMap (uncurry enlargeTile)
+      where
+        enlargeTile (x, y) r =
+          [ ( ( x + (dx * 100), -- probs shouldn't hard code
+                y + (dy * 100)
+              ),
+              newRisk r dx dy
+            )
+            | dx <- [0 .. 4],
+              dy <- [0 .. 4]
+          ]
+        -- probably can use `mod` in some way...
+        newRisk r dx dy =
+          let res = r + dx + dy
+           in if res >= 10 then res - 9 else res
 
 parseGrid :: [[Char]] -> (Grid, GridSize)
 parseGrid input = (M.fromList gridAsList, GS maxX maxY)
@@ -52,6 +87,3 @@ parseGrid input = (M.fromList gridAsList, GS maxX maxY)
       ]
     maxX = maximum . map (fst . fst) $ gridAsList
     maxY = maximum . map (snd . fst) $ gridAsList
-
-zipWithIndex :: [a] -> [(Int, a)]
-zipWithIndex = zip [0 ..]
