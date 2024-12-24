abb25674582457936c061ceff9f062639a76527f
Paul Stanford <pmstanford95@gmail.com>
2024-12-24 19:39:44 +0000

    reuse some of 2024 day 24 for 2015 day 7

--- src/2015/Day7.hs
+++ src/2015/Day7.hs
@@ -1,8 +1,14 @@
 module Day7 where
 
-import Data.Bits (xor, (.&.), (.|.))
-import Text.Parsec as P (char, choice, digit, letter, many1, newline, oneOf, sepBy, string, try, (<|>))
+import Data.Bits (complement, shiftL, shiftR, (.&.), (.|.))
+import Data.Foldable (Foldable (foldl'))
+import qualified Data.Map as M
+import Text.Parsec as P (choice, digit, letter, many1, newline, sepBy, string, try)
 import Text.ParserCombinators.Parsec (Parser, parse)
+
+type WireMap = M.Map String Int
+
+type GateMap = M.Map String Instruction
 
 data Instruction
   = INPUT Int String
@@ -12,6 +18,31 @@ data Instruction
   | RSHIFT String Int String
   | NOT String String
   deriving (Show)
+
+resolve :: [String] -> GateMap -> WireMap -> WireMap
+resolve wireNames gateMap wireMap =
+  foldl' (\acc wireName -> resolve' wireName gateMap acc) wireMap wireNames
+
+resolve' :: String -> GateMap -> WireMap -> WireMap
+resolve' wireName gateMap wireMap =
+  if M.member wireName wireMap
+    then wireMap
+    else case gateMap M.! wireName of
+      (INPUT x _) -> M.insert wireName x wireMap
+      (AND in1 in2 _) -> goBin in1 in2 (.&.)
+      (OR in1 in2 _) -> goBin in1 in2 (.|.)
+      (LSHIFT in1 x _) -> goSing in1 (shiftL x)
+      (RSHIFT in1 x _) -> goSing in1 (shiftR x)
+      (NOT in1 _) -> goSing in1 complement
+  where
+    resolveInput x = resolve' x gateMap wireMap
+    resolveInputs x y = resolve' x gateMap . resolve' y gateMap $ wireMap
+
+    insertNewSing r x op = M.insert wireName (op (r M.! x)) r
+    goSing x = insertNewSing (resolveInput x) x
+
+    insertNewBin r x y op = M.insert wireName ((r M.! x) `op` (r M.! y)) r
+    goBin x y = insertNewBin (resolveInputs x y) x y
 
 getInput :: IO [Instruction]
 getInput = unsafeParse inputParser <$> readFile "./fixtures/input7.txt"
@@ -25,11 +56,11 @@ inputParser :: Parser [Instruction]
 inputParser = instructionParser `sepBy` newline
 
 instructionParser :: Parser Instruction
-instructionParser = P.choice . map try $ [inputParser, andParser, orParser, lshiftParser, rshiftParser, notParser]
+instructionParser = P.choice . map try $ [wireInputParser, andParser, orParser, lshiftParser, rshiftParser, notParser]
   where
     -- 19138 -> b
-    inputParser :: Parser Instruction
-    inputParser =
+    wireInputParser :: Parser Instruction
+    wireInputParser =
       INPUT
         <$> (intParser <* string " -> ")
         <*> wireName
