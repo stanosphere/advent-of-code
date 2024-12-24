e74564ca715cdd2b92b2b7d8ed3eed6eb746a44f
Paul Stanford <pmstanford95@gmail.com>
2024-12-24 19:12:05 +0000

    finish off day 7 2015 parser

--- src/2015/Day7.hs
+++ src/2015/Day7.hs
@@ -1,51 +1,77 @@
 module Day7 where
 
-import Text.Parsec as P (char, choice, digit, letter, many1, newline, oneOf, sepBy, string, (<|>))
+import Data.Bits (xor, (.&.), (.|.))
+import Text.Parsec as P (char, choice, digit, letter, many1, newline, oneOf, sepBy, string, try, (<|>))
 import Text.ParserCombinators.Parsec (Parser, parse)
 
 data Instruction
   = INPUT Int String
   | AND String String String
   | OR String String String
-  | LSHIFT String String Int
-  | RSHIFT String String Int
+  | LSHIFT String Int String
+  | RSHIFT String Int String
   | NOT String String
+  deriving (Show)
+
+getInput :: IO [Instruction]
+getInput = unsafeParse inputParser <$> readFile "./fixtures/input7.txt"
+
+unsafeParse :: Parser a -> String -> a
+unsafeParse p s = case parse p "" s of
+  Left res -> error . show $ res
+  Right res -> res
+
+inputParser :: Parser [Instruction]
+inputParser = instructionParser `sepBy` newline
 
 instructionParser :: Parser Instruction
-instructionParser = P.choice [inputParser, andParser, orParser, lshiftParser, rshiftParser, notParser]
+instructionParser = P.choice . map try $ [inputParser, andParser, orParser, lshiftParser, rshiftParser, notParser]
+  where
+    -- 19138 -> b
+    inputParser :: Parser Instruction
+    inputParser =
+      INPUT
+        <$> (intParser <* string " -> ")
+        <*> wireName
 
--- 19138 -> b
-inputParser :: Parser Instruction
-inputParser = INPUT <$> intParser <* string " -> " <*> wireNameParser
+    -- hq AND hs -> ht
+    andParser :: Parser Instruction
+    andParser =
+      AND
+        <$> (wireName <* string " AND ")
+        <*> (wireName <* string " -> ")
+        <*> wireName
 
--- hq AND hs -> ht
-andParser :: Parser Instruction
-andParser = AND <$> wireNameParser <* string " AND " <*> wireNameParser <* string " -> " <*> wireNameParser
+    -- kg OR kf -> kh
+    orParser :: Parser Instruction
+    orParser =
+      OR
+        <$> (wireName <* string " OR ")
+        <*> (wireName <* string " -> ")
+        <*> wireName
 
--- kg OR kf -> kh
-orParser :: Parser Instruction
-orParser = OR <$> wireNameParser <* string " OR " <*> wireNameParser <* string " -> " <*> wireNameParser
+    lshiftParser :: Parser Instruction
+    lshiftParser =
+      LSHIFT
+        <$> (wireName <* string " LSHIFT ")
+        <*> (intParser <* string " -> ")
+        <*> wireName
 
-lshiftParser :: Parser Instruction
-lshiftParser = undefined
+    rshiftParser :: Parser Instruction
+    rshiftParser =
+      RSHIFT
+        <$> (wireName <* string " RSHIFT ")
+        <*> (intParser <* string " -> ")
+        <*> wireName
 
-rshiftParser :: Parser Instruction
-rshiftParser = undefined
+    notParser :: Parser Instruction
+    notParser =
+      NOT
+        <$> (string "NOT " *> wireName)
+        <*> (string " -> " *> wireName)
 
-notParser :: Parser Instruction
-notParser = undefined
+    intParser :: Parser Int
+    intParser = read <$> many1 digit
 
-intParser :: Parser Int
-intParser = read <$> many1 digit
-
-wireNameParser :: Parser String
-wireNameParser = many1 letter
-
--- 123 -> x
--- 456 -> y
--- x AND y -> d
--- x OR y -> e
--- x LSHIFT 2 -> f
--- y RSHIFT 2 -> g
--- NOT x -> h
--- NOT y -> i
+    wireName :: Parser String
+    wireName = many1 letter
