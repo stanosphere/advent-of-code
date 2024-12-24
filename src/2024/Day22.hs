8069979cb23a35705bf1f8e79c78e54ec5ddd0f9
Paul Stanford <pmstanford95@gmail.com>
2024-12-24 18:59:30 +0000

    brute force wont work lol

--- src/2024/Day22.hs
+++ src/2024/Day22.hs
@@ -2,10 +2,23 @@ module Day22 where
 
 import Data.Bits (Bits (xor))
 import Data.Char (digitToInt)
+import Data.Containers.ListUtils (nubOrd)
+import Data.Foldable (traverse_)
+import Data.List (elemIndex)
+import Data.Maybe (mapMaybe)
 import Utils.Grouping (windows)
 
 part1 :: IO Int
 part1 = sum . map (nSteps 2000) <$> getInput
+
+part2 = do
+  input <- getInput
+  let distinctChangeSequences = nubOrd . concatMap all4Sequences $ input
+  let res = map (\cs -> sum . mapMaybe (\i -> f cs i) $ input) distinctChangeSequences
+  print (maximum res)
+
+f :: [Int] -> Int -> Maybe Int
+f changeSequence i = fmap (\j -> lastDigit (nSteps j i)) (elemIndex changeSequence (all4Sequences i))
 
 getInput :: IO [Int]
 getInput = map read . lines <$> readFile "./fixtures/input22.txt"
@@ -13,25 +26,23 @@ getInput = map read . lines <$> readFile "./fixtures/input22.txt"
 nSteps :: Int -> Int -> Int
 nSteps n x = iterate step x !! n
 
+all4Sequences :: Int -> [[Int]]
+all4Sequences = windows 4 . mkChangeSequence
+
 mkChangeSequence :: Int -> [Int]
 mkChangeSequence = take 2000 . diffs . iterate step
   where
     diffs :: [Int] -> [Int]
     diffs = map (\y -> (lastDigit . last $ y) - (lastDigit . head $ y)) . windows 2
 
-    lastDigit :: Int -> Int
-    lastDigit = digitToInt . last . show
+lastDigit :: Int -> Int
+lastDigit = digitToInt . last . show
 
 step :: Int -> Int
 step = step3 . step2 . step1
   where
-    step1 :: Int -> Int
     step1 x = prune . mix (64 * x) $ x
-
-    step2 :: Int -> Int
     step2 x = prune . mix (floor (asDouble x / 32)) $ x
-
-    step3 :: Int -> Int
     step3 x = prune . mix (2048 * x) $ x
 
     asDouble :: (Integral a) => a -> Double
