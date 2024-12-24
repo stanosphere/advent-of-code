3a1cce78df9f604b8a5ed70b2139201db6784ff9
Paul Stanford <pmstanford95@gmail.com>
2024-12-24 18:06:55 +0000

    way to make change sequences

--- src/2024/Day22.hs
+++ src/2024/Day22.hs
@@ -1,6 +1,8 @@
 module Day22 where
 
 import Data.Bits (Bits (xor))
+import Data.Char (digitToInt)
+import Utils.Grouping (windows)
 
 part1 :: IO Int
 part1 = sum . map (nSteps 2000) <$> getInput
@@ -10,6 +12,15 @@ getInput = map read . lines <$> readFile "./fixtures/input22.txt"
 
 nSteps :: Int -> Int -> Int
 nSteps n x = iterate step x !! n
+
+mkChangeSequence :: Int -> [Int]
+mkChangeSequence = take 2000 . diffs . iterate step
+  where
+    diffs :: [Int] -> [Int]
+    diffs = map (\y -> (lastDigit . last $ y) - (lastDigit . head $ y)) . windows 2
+
+    lastDigit :: Int -> Int
+    lastDigit = digitToInt . last . show
 
 step :: Int -> Int
 step = step3 . step2 . step1
