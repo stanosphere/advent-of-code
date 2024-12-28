be10527dc0d4bec78cf119b1903f76147acaa6e9
paul <pmstanford95@gmail.com>
2024-12-28 12:35:48 +0000

    day 4 2016 part 2

--- src/2016/Day4.hs
+++ src/2016/Day4.hs
@@ -1,10 +1,11 @@
 module Day4 where
 
 import Data.Bifunctor (first)
+import Data.Foldable (traverse_)
 import Data.List (sort)
 import qualified Data.Map as M
 import Data.Tuple (swap)
-import Text.Parsec (between, char, digit, letter, many1, newline, parse, sepBy)
+import Text.Parsec (between, char, digit, letter, many1, newline, parse, sepBy, (<|>))
 import Text.ParserCombinators.Parsec (Parser)
 import Utils.Grouping (frequencies)
 
@@ -18,6 +19,21 @@ data Room = Room
 part1 :: IO Int
 part1 = sum . map _sectorId . filter isValid <$> getInput
 
+-- just manually search through when done
+part2 :: IO ()
+part2 = (>>= traverse_ print) $ map (\r -> (decrypt r, _sectorId r)) . filter isValid <$> getInput
+
+decrypt :: Room -> String
+decrypt (Room encryptedName sectorId _) = map (\x -> if x == '-' then ' ' else shiftChar sectorId x) encryptedName
+
+shiftChar :: Int -> Char -> Char
+shiftChar i x = intToChar M.! (((charToInt M.! x) + i) `mod` 26)
+  where
+    -- probably would be best to create the mapping only once at the start!
+    mapping = zip [0 ..] ['a' .. 'z']
+    intToChar = M.fromList mapping
+    charToInt = M.fromList . map swap $ mapping
+
 isValid :: Room -> Bool
 isValid (Room encryptedName _ checksum) =
   (== checksum)
@@ -27,6 +43,7 @@ isValid (Room encryptedName _ checksum) =
     . map (first (* (-1)) . swap)
     . M.toList
     . frequencies
+    . filter (/= '-')
     $ encryptedName
 
 getInput :: IO [Room]
@@ -36,7 +53,7 @@ getInput = unsafeParse inputParser <$> readFile "./fixtures/input4.txt"
 
     room = Room <$> encryptedName <*> int <*> checksum
 
-    encryptedName = concat <$> many1 (many1 letter <* char '-')
+    encryptedName = many1 (letter <|> char '-')
     int = read <$> many1 digit
     checksum = between (char '[') (char ']') (many1 letter)
 
