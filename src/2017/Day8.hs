d69f0f6e792d780567605818eb815d4e56652719
Paul Stanford <pmstanford95@gmail.com>
2025-11-19 18:44:40 +0000

    2017 day 8 part 1

--- src/2017/Day8.hs
+++ src/2017/Day8.hs
@@ -1,16 +1,23 @@
 module Day8 where
 
+import Data.Foldable (Foldable (foldl'))
 import Data.Functor (($>))
-import Text.Parsec as P (char, choice, digit, letter, many1, string, try, (<|>))
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
+import Text.Parsec as P (char, choice, digit, letter, many1, newline, sepBy, string, try, (<|>))
 import Text.ParserCombinators.Parsec (Parser, parse)
 import Prelude hiding (EQ, GT, LT)
 
-data Operand = EQ | LT | LEQ | GT | GEQ deriving (Show)
+data Operand = EQ | NEQ | LT | LEQ | GT | GEQ deriving (Show)
 
 data Action = Inc | Dec deriving (Show)
 
+-- I suppose this could be represented as a function, but I like being able to see the operand I guess
+-- like the parser absolutely could output a function with signature `Int -> Bool` given the comparator and operand
 data Condition = Condition {toCheck :: String, operand :: Operand, comparator :: Int} deriving (Show)
 
+-- again we probably don't need to store action and amount separately
+-- but a parser should probably just parse, and not have any other logic
 data Command = Command
   { toModify :: String,
     action :: Action,
@@ -18,6 +25,33 @@ data Command = Command
     cond :: Condition
   }
   deriving (Show)
+
+type Register = M.Map String Int
+
+part1 :: IO Int
+part1 = foldr max 0 . foldl' applyCommand M.empty <$> getInput
+
+applyCommand :: Register -> Command -> Register
+applyCommand reg command = if res then M.insertWith (+) (toModify command) valueToInsert reg else reg
+  where
+    condition = cond command
+    existingValue = fromMaybe 0 (reg M.!? toCheck condition)
+    comparatorValue = comparator condition
+    res = case operand condition of
+      EQ -> existingValue == comparatorValue
+      NEQ -> existingValue /= comparatorValue
+      LT -> existingValue < comparatorValue
+      LEQ -> existingValue <= comparatorValue
+      GT -> existingValue > comparatorValue
+      GEQ -> existingValue >= comparatorValue
+    valueToInsert = case action command of
+      Inc -> amount command
+      Dec -> (-1) * amount command
+
+getInput :: IO [Command]
+getInput = unsafeParse inputParser <$> readFile "./fixtures/input8.txt"
+  where
+    inputParser = commandParser `P.sepBy` P.newline
 
 commandParser :: Parser Command
 commandParser =
@@ -45,14 +79,16 @@ commandParser =
         <* P.char ' '
         <*> intParser
 
+    -- order here matters: you have to try `<=` before plain old `<` for example
     operandParser :: Parser Operand
     operandParser =
       P.choice . map P.try $
         [ P.string "==" $> EQ,
-          P.string "<" $> LT,
+          P.string "!=" $> NEQ,
           P.string "<=" $> LEQ,
-          P.string ">" $> GT,
-          P.string ">=" $> GEQ
+          P.string "<" $> LT,
+          P.string ">=" $> GEQ,
+          P.string ">" $> GT
         ]
 
 unsafeParse :: Parser a -> String -> a
