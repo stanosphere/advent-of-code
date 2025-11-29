100044d7c5546500986980279cf8b7e5bb9119cd
Paul Stanford <pmstanford95@gmail.com>
2025-11-29 16:02:31 +0000

    day 9 2017 part 2

--- src/2017/Day9.hs
+++ src/2017/Day9.hs
@@ -1,5 +1,6 @@
 module Day9 where
 
+import Data.Maybe (catMaybes)
 import Text.Parsec as P (anyChar, char, many, noneOf, sepBy, (<|>))
 import Text.ParserCombinators.Parsec (Parser, parse)
 
@@ -12,8 +13,18 @@ type Group = [GroupItem]
 part1 :: IO Int
 part1 = scoreGroup 1 . unsafeParse groupParser <$> getInput
 
+part2 :: IO Int
+part2 = countGarbage . unsafeParse groupParser <$> getInput
+
 getInput :: IO String
 getInput = readFile "./fixtures/input9.txt"
+
+countGarbage :: Group -> Int
+countGarbage = sum . map countGarbageInSubGroup
+  where
+    countGarbageInSubGroup :: GroupItem -> Int
+    countGarbageInSubGroup (Garbage str) = length str
+    countGarbageInSubGroup (SubGroup sg) = countGarbage sg
 
 scoreGroup :: Int -> Group -> Int
 scoreGroup currentScore = (currentScore +) . sum . map scoreSubGroup
@@ -33,11 +44,11 @@ groupParser = P.char '{' *> groupItemParser `sepBy` P.char ',' <* P.char '}'
 
     -- garbage starts with a `<` and ends with a `>`
     -- the '!' char is effectively an escape char
-    garbageParser :: Parser GroupItem
-    garbageParser = Garbage <$> (P.char '<' *> P.many (normalChar P.<|> escapedChar) <* P.char '>')
+    -- garbageParser :: Parser GroupItem
+    garbageParser = Garbage . catMaybes <$> (P.char '<' *> P.many (normalChar P.<|> escapedChar) <* P.char '>')
       where
-        normalChar = P.noneOf "!>"
-        escapedChar = P.char '!' *> P.anyChar
+        normalChar = Just <$> P.noneOf "!>"
+        escapedChar = Nothing <$ (P.char '!' *> P.anyChar)
 
 unsafeParse :: Parser a -> String -> a
 unsafeParse p s = case parse p "" s of
