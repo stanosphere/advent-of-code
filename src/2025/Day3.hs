a03c9f916ef6b41ef708a5de22165888ee32f7a7
Paul Stanford <pmstanford95@gmail.com>
2025-12-03 20:24:33 +0000

    2025 day 3 part 2

--- src/2025/Day3.hs
+++ src/2025/Day3.hs
@@ -1,6 +1,7 @@
 module Day3 where
 
-import Data.List.Extra (unsnoc)
+import Data.List.Extra (find, uncons, unsnoc)
+import Data.Maybe (mapMaybe)
 
 -- part 1
 -- - remove the last digit from the string
@@ -10,8 +11,42 @@ import Data.List.Extra (unsnoc)
 -- - find the biggest number
 -- - you have your answer
 
+-- part 2
+-- - kind of the same as part 1 but you get rid of 11 digits, then 10, etc
+
+data SolutionState = SolutionState
+  { _found :: [Char], -- the digits we've found so far, intially empty list
+    _start :: [Char], -- the start of the string were we get digits from
+    _end :: [Char] -- digits we're not including in our search yet
+  }
+
 part1 :: IO Int
 part1 = sum . map solve <$> getInput
+
+part1' :: IO Int
+part1' = sum . mapMaybe (solve' 2) <$> getInput
+
+part2 :: IO Int
+part2 = sum . mapMaybe (solve' 12) <$> getInput
+
+solve' :: Int -> [Char] -> Maybe Int
+solve' digits row = fmap (read . _found) . find ((== digits) . length . _found) . iterate step $ SolutionState [] start end
+  where
+    len = length row
+    -- probably there's a splitAt or something?
+    start = take (len - digits + 1) row
+    end = drop (len - digits + 1) row
+
+-- I kind of think vectors would be nicer because there's a lot of concatenations and un-concatenations going on
+step :: SolutionState -> SolutionState
+step (SolutionState found start end) =
+  if null end
+    then SolutionState (found ++ [nextFound]) [] []
+    else SolutionState (found ++ [nextFound]) nextStart nextEnd
+  where
+    nextFound = maximum start
+    (headEnd, nextEnd) = unsafeGet . uncons $ end
+    nextStart = (++ [headEnd]) . drop 1 . dropWhile (/= nextFound) $ start
 
 solve :: String -> Int
 solve s = read [tensDigit, unitsDigit]
