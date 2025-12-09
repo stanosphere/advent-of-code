882a267bcb15c3f2dd0e18aa54e165398f3f64b4
Paul Stanford <pmstanford95@gmail.com>
2025-12-09 23:59:11 +0000

    2025 day 9 part 2

--- src/2025/Day9.hs
+++ src/2025/Day9.hs
@@ -5,6 +5,45 @@ import Text.ParserCombinators.Parsec (Parser, parse)
 
 type Coords = (Int, Int)
 
+-- oof part 2 is gonna be nasty
+-- https://aoc.oppi.li/2.5-day-9.html#day-9
+-- I took some inspiration from the above!
+
+part2 :: IO Int
+part2 = solvePart2 <$> getInput
+
+solvePart2 :: [Coords] -> Int
+solvePart2 loopCoords =
+  maximum
+    . map (uncurry rectArea)
+    . filter (uncurry (rectWithinLoop allSidesOfLoop))
+    . allPairs
+    $ loopCoords
+  where
+    allSidesOfLoop = sides loopCoords
+    sides (p : ps) = zip (p : ps) (ps ++ [p])
+    sides [] = []
+
+rectWithinLoop :: [(Coords, Coords)] -> Coords -> Coords -> Bool
+rectWithinLoop allSidesOfLoop (x1, y1) (x2, y2) = not . rectOutsideLoop allSidesOfLoop (x1, y1) $ (x2, y2)
+
+-- we check our rect against each side in the loop
+-- if it ever intersects one of the sides we know we've crossed the loop!
+-- so for each side we can make that check
+-- a side is actually just a rectangle
+-- and there's a relatively well known formula for computing rect intersections
+-- it's essentially just 2 interval intersections: one for x dir, and one for y dir
+rectOutsideLoop :: [(Coords, Coords)] -> Coords -> Coords -> Bool
+rectOutsideLoop allSidesOfLoop (rx1, ry1) (rx2, ry2) = any sideIntersectsRect allSidesOfLoop
+  where
+    sideIntersectsRect ((sx1, sy1), (sx2, sy2)) =
+      and
+        [ max sx1 sx2 > min rx1 rx2,
+          min sx1 sx2 < max rx1 rx2,
+          max sy1 sy2 > min ry1 ry2,
+          min sy1 sy2 < max ry1 ry2
+        ]
+
 part1 :: IO Int
 part1 = maximum . map (uncurry rectArea) . allPairs <$> getInput
 
